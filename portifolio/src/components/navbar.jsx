89c4ac7a0174b48f883f4f7b6ff5bf771d3a3c81
Yijie Zou <yijiezou2004@gmail.com>
2024-06-12 19:19:50 -0600

    nav bar styling

--- portifolio/src/components/navbar.jsx
+++ portifolio/src/components/navbar.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
-import Logo from '../assets/logo.png'
+import Logo from '../assets/logo-white.png'
 
 
 const Navbar = () => {
   return (
-    <div>
-        <div className='w-96 bg-black'>
-            <img src={Logo} alt='logo' className='flex object-scale-down h-48'/>
+    <div className='flex w-full justify-between'>
+        <div className='flex w-1/5 bg-black justify-center items-center'>
+            <img src={Logo} alt='logo' className='flex object-scale-down  max-h-[90px]'/>
         </div>
 
-        <ul className='bg-gray-600 w-1/6'>
+
+
+        <ul className='flex w-4/5 bg-gray-800 justify-between items-center '>
             {['about', 'work', 'skills', 'contact'].map((item)=>(
-                <li key={`link-${item}`} className='pl-4 capitalize text-lg font-semibold'>
+                // <div>
+                <li key={`link-${item}`} className='mx-2 my-2 w-full py-1 uppercase text-center  ease-in-out duration-200 hover:bg-gray-400 hover:border-gray-300'>
                     <div />
-                    <a href={`#${item}`}>{item}</a>
+                    <a className='text-base sm:text-[10px] lg:text-xl text-white' href={`#${item}`}>{item}</a>
                 </li>
+                
             ))}
         </ul>
 
